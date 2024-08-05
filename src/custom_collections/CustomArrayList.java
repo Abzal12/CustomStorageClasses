351f1b31d8f7ef1c9f79e8df9a5e718a59449d26
Abzal <abzal0707@gmail.com>
2024-08-05 14:20:23 +0600

    Add minor modifications

--- src/custom_collections/CustomArrayList.java
+++ src/custom_collections/CustomArrayList.java
@@ -3,30 +3,29 @@ package src.custom_collections;
 import java.util.Arrays;
 
 public class CustomArrayList<T>{
-    private Object[] elements;
-    private int size = 0;
+    private Object[] elementSpaces;
+    private int actualSize = 0;
     private static final int DEFAULT_CAPACITY = 10;
 
     public CustomArrayList() {
-        elements = new Object[DEFAULT_CAPACITY];
+        elementSpaces = new Object[DEFAULT_CAPACITY];
     }
 
     private void ensureCapacity() {
-        if (size == elements.length) {
-            elements = Arrays.copyOf(elements, elements.length * 2);
+        if (actualSize == elementSpaces.length) {
+            elementSpaces = Arrays.copyOf(elementSpaces, elementSpaces.length * 2);
         }
     }
 
     public void put(T element) {
         ensureCapacity();
-        elements[size++] = element;
+        elementSpaces[actualSize++] = element;
     }
 
-    @SuppressWarnings("unchecked")
     public T get(int index) {
 
         try {
-            return (T) elements[index];
+            return (T) elementSpaces[index];
         } catch (IndexOutOfBoundsException e) {
             System.err.println(e.getMessage());
             return null;
@@ -39,11 +38,11 @@ public class CustomArrayList<T>{
         T oldElement = null;
         try {
             oldElement = get(index);
-            int numMoved = size - index - 1;
+            int numMoved = actualSize - index - 1;
             if (numMoved > 0) {
-                System.arraycopy(elements, index + 1, elements, index, numMoved);
+                System.arraycopy(elementSpaces, index + 1, elementSpaces, index, numMoved);
             }
-            elements[--size] = null;
+            elementSpaces[--actualSize] = null;
         } catch (IndexOutOfBoundsException | NullPointerException e) {
             System.err.println(e.getMessage());
         }
@@ -51,23 +50,22 @@ public class CustomArrayList<T>{
     }
 
     public int size() {
-        return size;
+        return actualSize;
     }
 
-    public Object[] getElements() {
-        return elements;
+    public Object[] getElementSpaces() {
+        return elementSpaces;
     }
 
     public void showInstanceStateBehaviourAbilities() {
         System.out.println("CustomArrayList instance state:");
         System.out.println("Initial capacity by default: " + DEFAULT_CAPACITY);
         System.out.println("Current array list size is: " + size());
-        System.out.println("Current capacity: " + elements.length);
+        System.out.println("Current capacity: " + getElementSpaces().length);
         System.out.println();
         System.out.println("CustomArrayList instance behaviour:");
         System.out.println("Querying 2nd element: " + get(1));
         System.out.println("Deleting 2nd element: " + delete(1));
         System.out.println("Querying 2nd element: " + get(1));
-
     }
 }
