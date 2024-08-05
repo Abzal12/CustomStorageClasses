458f898c6d5abfed3221836bde6b182319a6d8d4
Abzal <abzal0707@gmail.com>
2024-08-05 14:25:21 +0600

    Add minor modifications

--- src/services/FileDataReader.java
+++ src/services/FileDataReader.java
@@ -10,6 +10,7 @@ public class FileDataReader {
 
     public static void fulfillListFromFile(String pathToData, CustomArrayList<String> names) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(pathToData))) {
+            System.out.println("Fulfilling a custom ArrayList with some data from a txt file ...");
             String name;
             while ((name = bufferedReader.readLine()) != null) {
                 names.put(name);
